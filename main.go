68a9c00126593d01cede4eb9c6dd0f9d672951c6
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-08-20 13:13:36 +0900

    Add replace(-i) option

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -21,6 +22,44 @@ func filter(reader io.Reader, bw *bufio.Writer) error {
 			fmt.Fprintf(bw, "&#x%X;", int(r))
 		} else {
 			bw.WriteRune(r)
+		}
+	}
+	return nil
+}
+
+var flagReplace = flag.Bool("i", false, "replace file")
+
+func mainsReplace(args []string) error {
+	for _, arg1 := range args {
+		r, err := os.Open(arg1)
+		if err != nil {
+			return err
+		}
+		newFile := arg1 + ".tmp"
+		w, err := os.Create(newFile)
+		if err != nil {
+			return err
+		}
+		bw := bufio.NewWriter(w)
+		filterErr := filter(r, bw)
+		bw.Flush()
+		rCloseErr := r.Close()
+		wCloseErr := w.Close()
+
+		if filterErr != nil {
+			return filterErr
+		}
+		if rCloseErr != nil {
+			return rCloseErr
+		}
+		if wCloseErr != nil {
+			return wCloseErr
+		}
+		if err = os.Rename(arg1, arg1+"~"); err != nil {
+			return err
+		}
+		if err = os.Rename(newFile, arg1); err != nil {
+			return err
 		}
 	}
 	return nil
@@ -51,7 +90,14 @@ func mains(args []string) error {
 }
 
 func main() {
-	if err := mains(os.Args[1:]); err != nil {
+	var err error
+	flag.Parse()
+	if *flagReplace {
+		err = mainsReplace(flag.Args())
+	} else {
+		err = mains(flag.Args())
+	}
+	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
